3e21c05259eb5f49d70d0c2a30162363c7768d4e
Gninninwoky Ouattara <idrissgouattara@gmail.com>
2021-08-02 16:20:42 +0000

    now render two columns scrollabe list of categories

--- screens/CategoriesScreen.tsx
+++ screens/CategoriesScreen.tsx
@@ -1,17 +1,50 @@
 import React from "react";
-import { View, Text, StyleSheet, Button } from "react-native";
+import { View, Text, StyleSheet, Button, FlatList } from "react-native";
+
+import { CATEGORIES } from "../data/dummy-data";
+
+// Item Components
+
+type itemProps = {
+    title: any;
+};
+
+const Item: React.FC<itemProps> = ({ title }) => (
+    <View>
+        <Text>{title}</Text>
+    </View>
+);
+
+// CategoriesScreen Component
+
+type renderItemType = (item: { item: { title: string } }) => JSX.Element;
 
 const CategoriesScreen = (props: any) => {
-    return (
-        <View style={styles.screen}>
-            <Text>The Categories screen</Text>
-            <Button
-                title="Go to meals"
-                onPress={() => {
-                    props.navigation.navigate({ routeName: "CategoryMeals" });
-                }}
-            />
+    const renderItem: renderItemType = ({ item }) => (
+        <View style={styles.gridItem}>
+            <Item title={item.title} />
         </View>
+    );
+
+    // return (
+    //     <View style={styles.screen}>
+    //         <Text>The Categories screen</Text>
+    //         <Button
+    //             title="Go to meals"
+    //             onPress={() => {
+    //                 props.navigation.navigate({ routeName: "CategoryMeals" });
+    //             }}
+    //         />
+    //     </View>
+    // );
+
+    return (
+        <FlatList
+            numColumns={2}
+            data={CATEGORIES}
+            renderItem={renderItem}
+            keyExtractor={(item) => item.id}
+        />
     );
 };
 
@@ -20,6 +53,11 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
+    },
+    gridItem: {
+        flex: 1,
+        margin: 15,
+        height: 150,
     },
 });
 
