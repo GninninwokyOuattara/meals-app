effa50b4b9449c16d29ff558f44b676f34c134ff
Gninninwoky Ouattara <idrissgouattara@gmail.com>
2021-08-03 15:25:32 +0000

    basic render of meals item

--- screens/CategoryMealsScreen.tsx
+++ screens/CategoryMealsScreen.tsx
@@ -1,26 +1,25 @@
 import React from "react";
-import { View, Text, StyleSheet, Button } from "react-native";
-import { CATEGORIES } from "../data/dummy-data";
+import { View, Text, StyleSheet, Button, FlatList } from "react-native";
+import { CATEGORIES, MEALS } from "../data/dummy-data";
+import Meal from "../models/Meal";
 
 const CategoryMealsScreen = (props: any) => {
+    const renderMealItem = (itemData: any) => {
+        console.log(itemData.item.title);
+        return (
+            <View>
+                <Text>{itemData.item.title}</Text>
+            </View>
+        );
+    };
+
     const categoryId = props.navigation.getParam("categoryId");
-    const selectedCategory = CATEGORIES.find(
-        (category) => category.id === categoryId
+
+    const displayedMeals = MEALS.filter(
+        (meal) => meal.categoryIds.indexOf(categoryId) >= 0
     );
 
-    return (
-        <View style={styles.screen}>
-            <Text>The Categories Meals screen</Text>
-            <Text>{selectedCategory?.title}</Text>
-
-            <Button
-                title="Go to Details"
-                onPress={() => {
-                    props.navigation.navigate({ routeName: "MealsDetails" });
-                }}
-            />
-        </View>
-    );
+    return <FlatList data={displayedMeals} renderItem={renderMealItem} />;
 };
 
 CategoryMealsScreen.navigationOptions = (navigation: any) => {
