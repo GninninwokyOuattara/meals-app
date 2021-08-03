1166ed78ccf9eb943889336cdd34a038640439b1
Gninninwoky Ouattara <idrissgouattara@gmail.com>
2021-08-03 23:35:16 +0000

    set header to the current meal navigated to

--- screens/MealDetailsScreen.tsx
+++ screens/MealDetailsScreen.tsx
@@ -1,12 +1,23 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
+import { MEALS } from "../data/dummy-data";
 
-const MealsDetailsScreen = () => {
+const MealsDetailsScreen = (props: any) => {
+    const mealId = props.navigation.getParam("MealId");
+    const selectedMeal = MEALS.find((meal) => meal.id === mealId);
     return (
         <View style={styles.screen}>
             <Text>The Meal Details Screen screen</Text>
         </View>
     );
+};
+
+MealsDetailsScreen.navigationOptions = (props: any) => {
+    const mealId = props.navigation.getParam("MealId");
+    const selectedMeal = MEALS.find((meal) => meal.id === mealId);
+    return {
+        headerTitle: selectedMeal?.title,
+    };
 };
 
 const styles = StyleSheet.create({
