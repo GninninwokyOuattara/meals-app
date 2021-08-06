9fbfef8b0af7452311045daa8e56093b108f944f
Gninninwoky Ouattara <idrissgouattara@gmail.com>
2021-08-06 14:44:20 +0000

    add some filters that do nothing yet beside being toggable

--- screens/FiltersScreen.tsx
+++ screens/FiltersScreen.tsx
@@ -1,13 +1,65 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { View, Text, StyleSheet, Switch } from "react-native";
 import { HeaderButtons, Item } from "react-navigation-header-buttons";
 import CustomHeaderButton from "../components/HeaderButton";
 import COLORS from "../constants/Colors";
 
+const Filter = (props: {
+    title: string;
+    state: boolean;
+    setStateFunction: React.Dispatch<React.SetStateAction<boolean>>;
+}) => {
+    return (
+        <View style={styles.filterContainer}>
+            <Text style={styles.filterTitle}>{props.title}</Text>
+            <Switch
+                trackColor={{ true: COLORS.accentColor, false: "white" }}
+                onValueChange={(value) => props.setStateFunction(value)}
+                value={props.state}
+            />
+        </View>
+    );
+};
+
 const FiltersScreen = () => {
+    const [isGlutenFree, setIsGutenFree] = useState(false);
+    const [isLactoreFree, setIsLactoseFree] = useState(false);
+    const [isVegan, setIsVegan] = useState(false);
+    const [isVegeratian, setIsVegetarian] = useState(false);
+
     return (
         <View style={styles.screen}>
-            <Text>The Filter screen</Text>
+            <Text
+                style={{
+                    fontFamily: "OpenSans-Bold",
+                    fontSize: 20,
+                    alignSelf: "center",
+                    marginBottom: 15,
+                }}
+            >
+                Available Filters / Restrictions
+            </Text>
+
+            <Filter
+                title={"Gluten-Free"}
+                state={isGlutenFree}
+                setStateFunction={setIsGutenFree}
+            />
+            <Filter
+                title={"Lactore-Free"}
+                state={isLactoreFree}
+                setStateFunction={setIsLactoseFree}
+            />
+            <Filter
+                title={"Vegan"}
+                state={isVegan}
+                setStateFunction={setIsVegan}
+            />
+            <Filter
+                title={"Vegatarian"}
+                state={isVegeratian}
+                setStateFunction={setIsVegetarian}
+            />
         </View>
     );
 };
@@ -33,8 +85,19 @@ FiltersScreen.navigationOptions = (props: any) => {
 const styles = StyleSheet.create({
     screen: {
         flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
+        // justifyContent: "center",
+        // alignItems: "center",
+        padding: 10,
+    },
+    filterContainer: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        paddingHorizontal: 20,
+        marginVertical: 10,
+    },
+    filterTitle: {
+        fontFamily: "OpenSans-Bold",
+        fontSize: 15,
     },
 });
 
