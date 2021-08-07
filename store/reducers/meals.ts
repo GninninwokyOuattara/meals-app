71bb5aaeceb474cb68721ca3e91a8a68fec74ff4
Gninninwoky Ouattara <idrissgouattara@gmail.com>
2021-08-07 22:47:50 +0000

    meal reducer

--- store/reducers/meals.ts
+++ store/reducers/meals.ts
@@ -1,12 +1,11 @@
 import { MEALS } from "../../data/dummy-data";
+import Meal from "../../models/Meal";
 
 const initialState = {
     meals: MEALS,
     filteredMeals: MEALS,
     favoriteMeals: [],
 };
-
-// type InitState = typeof initialState
 
 const mealsReducer = (state = initialState, actions: any) => {
     return state;
