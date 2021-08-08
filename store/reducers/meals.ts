924b61221f35ffc4f8b612ac461dcd8d00613b92
Gninninwoky Ouattara <idrissgouattara@gmail.com>
2021-08-08 22:17:30 +0000

    first action, to fav and unfav meal

--- store/reducers/meals.ts
+++ store/reducers/meals.ts
@@ -1,13 +1,44 @@
 import { MEALS } from "../../data/dummy-data";
 import Meal from "../../models/Meal";
+import { TOGGLE_FAVORITE } from "../actions/meals";
+import { Reducer } from "redux";
 
-const initialState = {
+const initialState: {
+    meals: Meal[];
+    filteredMeals: Meal[];
+    favoriteMeals: Meal[];
+} = {
     meals: MEALS,
     filteredMeals: MEALS,
     favoriteMeals: [],
 };
 
 const mealsReducer = (state = initialState, actions: any) => {
+    switch (actions.type) {
+        case TOGGLE_FAVORITE:
+            const idx = state.favoriteMeals.findIndex(
+                (meal: Meal) => meal.id === actions.mealId
+            );
+            if (idx >= 0) {
+                let updatedFav = [...state.favoriteMeals];
+                updatedFav.splice(idx, 1);
+                return { ...state, favoriteMeals: updatedFav };
+            } else {
+                let meal = state.meals.find(
+                    (meal: Meal) => meal.id === actions.mealId
+                );
+                if (meal) {
+                    let updatedFav = [...state.favoriteMeals, meal];
+                    return {
+                        ...state,
+                        favoriteMeals: updatedFav,
+                    };
+                }
+            }
+
+        default:
+            return state;
+    }
     return state;
 };
 
