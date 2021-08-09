96145b7c82f22d2454365b95fe28dba0bd8621cb
Gninninwoky Ouattara <idrissgouattara@gmail.com>
2021-08-09 14:24:48 +0000

    add type

--- types.ts
+++ types.ts
@@ -1,3 +1,10 @@
 import { store } from "./App";
 
 export type RootState = ReturnType<typeof store.getState>;
+
+export type FilterName =
+    | "isGlutenFree"
+    | "isLactoseFree"
+    | "isVegan"
+    | "isVegetarian";
+export type Filter = { [key in FilterName]: boolean };
