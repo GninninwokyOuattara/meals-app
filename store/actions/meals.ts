3916b2ce6ce273cf971c6adeed5960745988b5a1
Gninninwoky Ouattara <idrissgouattara@gmail.com>
2021-08-09 14:26:16 +0000

    toggle filter action

--- store/actions/meals.ts
+++ store/actions/meals.ts
@@ -1,5 +1,13 @@
+import { Filter } from "../../types";
+
 export const TOGGLE_FAVORITE = "TOGGLE_FAVORITE";
 
 export const toggleFavorite = (id: string) => {
     return { type: TOGGLE_FAVORITE, mealId: id };
+};
+
+export const TOGGLE_FILTER = "TOGGLE_FILTER";
+
+export const toggleFilter = (filters: Filter) => {
+    return { type: TOGGLE_FILTER, filters };
 };
